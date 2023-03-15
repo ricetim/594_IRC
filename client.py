6f2da0cce83a591b87ce9f8ba1cd78690fb9c58d
Tim Rice <whoneedsgravity@gmail.com>
2023-03-14 20:56:57 -0700

    initial commit

--- client.py
+++ client.py
@@ -1,1 +1,17 @@
-cod
+import socket
+
+class irc_client():
+    
+    def start(self, name):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((socket.gethostname(), 55555))
+
+
+
+def main():
+    # name = input("Please enter your name: ")
+    client = irc_client()
+    client.start('tim')
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,39 @@
+import socket
+
+
+
+class irc_server():
+    
+    def __init_(self, host_addr, host_port):
+        # self.host_addr = host_addr
+        # self.port = host_port
+        self.host_addr = '127.0.0.1'
+        self.port = 99999
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((socket.gethostname(), self.host_port))
+        s.listen(5)
+        
+        while True:
+            client_socket, client_address = s.accept()
+            print(f"Connection from {client_address} has been established.")
+            
+    def start(self, host_addr, host_port):
+        # self.host_addr = host_addr
+        # self.port = host_port
+        self.host_addr = '127.0.0.1'
+        self.host_port = 55555
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((socket.gethostname(), self.host_port))
+        s.listen(5)
+        
+        while True:
+            client_socket, client_address = s.accept()
+            print(f"Connection from {client_address} has been established.")
+
+
+def main():
+    server = irc_server()
+    server.start('127.0.0.1', 55555)
+    
+if __name__ == "__main__":
+    main()
